a6ff025be4d9b2934bb8efed90df222ec3d02469
David <96037054+DavidChili34@users.noreply.github.com>
2022-12-26 21:18:48 +0500

    Create

---
+++ detection/object_detection.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import os
+
+
+def detect_object(frame):
+    cfg_path = os.path.abspath('yolo/yolov4.cfg')
+    weights_path = os.path.abspath('yolo/yolov4.weights')
+    names_path = os.path.abspath('yolo/coco.names')
+
+    net = cv2.dnn_DetectionModel(cfg_path, weights_path)
+    net.setInputSize(704, 704)
+    net.setInputScale(1.0 / 255)
+    net.setInputSwapRB(True)
+
+    with open(names_path, 'rt') as f:
+        names = f.read().rstrip('\n').split('\n')
+
+    classes, confidences, boxes = net.detect(frame, confThreshold=0.1, nmsThreshold=0.4)
+    for classId, confidence, box in zip(classes.flatten(), confidences.flatten(), boxes):
+        label = '%.2f' % confidence
+        label = '%s: %s' % (names[classId], label)
+        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        left, top, width, height = box
+        top = max(top, labelSize[1])
+        cv2.rectangle(frame, box, color=(0, 255, 0), thickness=3)
+        cv2.rectangle(frame, (left, top - labelSize[1]), (left + labelSize[0], top + baseLine),
+                      (255, 255, 255), cv2.FILLED)
+        cv2.putText(frame, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+    return frame
---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import io
+from PIL import Image
+import numpy as np
+from detection.object_detection import detect_object
+
+
+def main():
+    st.title("Распознование объектов с помощью YOLOv4")
+    img_array = load_image()
+    result = st.button('Распознать изображение')
+    if result:
+        if isinstance(img_array, np.ndarray):
+            image = detect_object(img_array)
+            st.write('**Результаты распознавания:**')
+            st.image(image)
+
+
+def load_image():
+    uploaded_image = st.file_uploader("Пожалуйста выберите фото", type=["png", "jpg", "jpeg"])
+    if uploaded_image is not None:
+        try:
+            image = Image.open(uploaded_image)
+        except Exception:
+            st.error("Error: Неверная фотография")
+        else:
+            image = image.convert("RGB")
+            img_array = np.array(image)
+            return img_array
+
+
+if __name__ == '__main__':
+    main()
